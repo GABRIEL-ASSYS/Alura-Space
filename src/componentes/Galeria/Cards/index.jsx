35156510c4f5cbfa970bfb5731bb6135355befce
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-05-16 09:57:28 -0300

    Update index.jsx

--- src/componentes/Galeria/Cards/index.jsx
+++ src/componentes/Galeria/Cards/index.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import open from './open.png'
 import favorito from './favorito.png'
 
-export default function Cards( { itens, styles } ) {
+export default function Cards({ itens, styles }) {
     return (
-        <ul className={styles.galeria__lista}>
+        <ul className={styles.galeria__cards}>
             {itens.map((item) => {
                 return (
                     <li key={item.id} className={styles.galeria__card}>
-                        <img src={item.imagem} alt={foto.titulo} className={styles.galeria__imagem} />
+                        <img src={item.imagem} alt={item.titulo} className={styles.galeria__imagem} />
+                        <p className={styles.galeria__descricao}>{item.titulo}</p>
+                        <div>
+                            <p>{item.creditos}</p>
+                            <span>
+                                <img src={open} alt="ícone de coração curtir" />
+                                <img src={favorito} alt="ícone de abrir modal" />
+                            </span>
+                        </div>
                     </li>
-                    ,
-                    <p className={styles.galeria__descricao}>{item.titulo}</p>
-                    ,
-                    <div>
-                        <p>{item.creditos}</p>
-                        <span>
-                            <img src={open} alt="ícone de coração curtir" />
-                            <img src={favorito} alt="ícone de abrir modal" />
-                        </span>
-                    </div>
                 )
             })}
         </ul>
